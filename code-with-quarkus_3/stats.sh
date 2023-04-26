2548142a99c911b9c69b7a0c9d7316de3dd51acf
Michal Karm Babacek <karm@redhat.com>
2023-04-26 15:13:04 +0200

    Comment

--- code-with-quarkus_3/stats.sh
+++ code-with-quarkus_3/stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# e.g. TAG=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3-java17-3 URL=http://127.0.0.1:8080/api/v1/image-stats ./stats.sh 
 bs=$(find -name "*quarkus.json")
 ts=$(find -name "*runner-timing-stats.json")
 stat_id=$(curl -s -w '\n' -H "Content-Type: application/json" -H "token: $TOKEN" --post302 --data "@$bs" "$URL/import?t=$TAG" | jq .id)
