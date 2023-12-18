0eab1bc84bd665bee5fb134ca26ede6705f9f361
Michal Karm Babacek <karm@redhat.com>
2023-12-18 23:21:52 +0100

    test

--- code-with-quarkus_3/run.sh
+++ code-with-quarkus_3/run.sh
@@ -1,23 +1,28 @@
-
 #!/bin/sh
 
 images=(
-"quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3.2.0-Final-java17" \
-"quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3.1.0-Final-java17" \
-"quay.io/quarkus/ubi-quarkus-graalvmce-builder-image:22.3.1-java19" \
-"quay.io/quarkus/ubi-quarkus-graalvmce-builder-image:22.3.1-java17" \
-"quay.io/karmkarm/ubi-quarkus-mandrel-builder-image:23.0-java20"
+"quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3.2.1-Final-java17"
+#"quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3.1.0-Final-java17" \
+#"quay.io/quarkus/ubi-quarkus-graalvmce-builder-image:22.3.1-java19" \
+#"quay.io/quarkus/ubi-quarkus-graalvmce-builder-image:22.3.1-java17" \
+#"quay.io/karmkarm/ubi-quarkus-mandrel-builder-image:23.0-java20"
+)
+
+qversions=(
+"3.0.4.Final"
 )
 
 for image in "${images[@]}"; do
-    echo $image;
+  for qversion in "${qversions[@]}"; do
+    echo $image $qversion;
     ./mvnw clean package \
         -Pnative -Dquarkus.platform.group-id=io.quarkus \
-        -Dquarkus.platform.version=3.0.0.Final \
+        -Dquarkus.platform.version=${qversion} \
         -Dquarkus.native.additional-build-args=-H:BuildOutputJSONFile=quarkus.json \
         -Dquarkus.native.container-build=true \
         -Dquarkus.native.builder-image=${image}
     pushd target
-    TAG=${image} URL=http://127.0.0.1:8080/api/v1/image-stats ../stats.sh
+    TAG="${image},${qversion}" URL=http://127.0.0.1:8080/api/v1/image-stats ../stats.sh
     popd
+  done
 done
---
+++ jakarta-ws-rs-core-httpheaders/common/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import jakarta.ws.rs.Path;
+import some.other.MyRealApp;
+
+@Path("")
+public class GreetingResource extends MyRealApp {
+}
