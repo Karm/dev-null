bb94d95273ae7d4ced1011cd48a37358eb71db78
Michal Karm Babacek <karm@redhat.com>
2024-03-22 23:25:26 +0100

    Reproducing OT JWT REST thing

---
+++ reproducer87326/src/main/java/biz/karms/meh/ProtectedResource.java
@@ -0,0 +1,25 @@
+package biz.karms.meh;
+
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.ClaimValue;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/protected")
+@RequestScoped
+public class ProtectedResource {
+
+    @Inject
+    @Claim("custom-value")
+    ClaimValue<String> custom;
+
+    @GET
+    @RolesAllowed("protected")
+    public String getJWTBasedValue() {
+        return "PROTECTED: " + custom.getValue();
+    }
+}
---
+++ reproducer87326/src/main/java/biz/karms/meh/Resource.java
@@ -0,0 +1,74 @@
+package biz.karms.meh;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.JWTOptions;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.UUID;
+
+@Path("/secured")
+@ApplicationScoped
+public class Resource {
+
+    private String key;
+
+    @PostConstruct
+    public void init() {
+        try (final InputStream is = Objects
+                .requireNonNull(Resource.class.getClassLoader().getResourceAsStream("/privateKey.pem"))) {
+            key = new String(is.readAllBytes(), StandardCharsets.US_ASCII);
+        } catch (Exception e) {
+            key = null;
+        }
+    }
+
+    @ConfigProperty(name = "quarkus.http.port")
+    int port;
+
+    @GET
+    @Path("/test")
+    public String testSecureCall() {
+        if (key == null) {
+            throw new WebApplicationException("Unable to read privateKey.pem", 500);
+        }
+        try (final Client c = ClientBuilder.newClient();
+                final Response response = c.target("http://localhost:" + port + "/protected")
+                        .request().header("authorization", "Bearer " + generateJWT(key))
+                        .buildGet().invoke()) {
+            return String.format("Claim value within JWT of 'custom-value' : %s", response.readEntity(String.class));
+        }
+    }
+
+    private static String generateJWT(String key) {
+        final JsonObject jsonObject = new JsonObject();
+        jsonObject.put("iss", "https://server.example.com");
+        jsonObject.put("sub", "XXX");
+        jsonObject.put("aud", "targetService");
+        jsonObject.put("jti", UUID.randomUUID().toString());
+        jsonObject.put("iat", System.currentTimeMillis() / 1000);
+        jsonObject.put("exp", (System.currentTimeMillis() + 30000) / 1000);
+        jsonObject.put("upn", "XXX");
+        jsonObject.put("custom-value", "My value");
+        final JsonArray jsonArray = new JsonArray();
+        jsonArray.add("user");
+        jsonArray.add("protected");
+        jsonObject.put("groups", jsonArray);
+        final JWTAuth provider = JWTAuth.create(null, new JWTAuthOptions().addPubSecKey(new PubSecKeyOptions().setAlgorithm("RS256").setBuffer(key)));
+        return provider.generateToken(new io.vertx.core.json.JsonObject().mergeIn(jsonObject), new JWTOptions().setAlgorithm("RS256"));
+    }
+}
